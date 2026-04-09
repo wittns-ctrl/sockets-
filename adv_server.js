fb3f40e262239700e324f7d2e0e2b577b54e694f
wittns <mugishawitness15@gmail.com>
2026-04-09 13:55:29 +0200

    adding targetted approach

--- adv_server.js
+++ adv_server.js
@@ -11,15 +11,19 @@ const server = app.listen(PORT,()=>{
 
 const wss = new WebSocketServer({server})
 
-wss.on('connection',(ws)=>{
+wss.on('connection',(ws,req)=>{
     console.log('handshake upgraded, new client connected')
 
+    const parameters= new URLSearchParams(req.url.split('?')[1])
+    const hallId = parameters.get('hallId')
+
+    ws.currenthall = hallId
     ws.on('message',(data)=>{
         console.log(`Server Received: ${data}`)
 
         const message = data.toString()
       wss.clients.forEach((client)=>{
-        if(client !== ws && client.readyState === 1){
+        if(client.currenthall === ws.currenthall && client !== ws && client.readyState === 1){
             client.send(`new update: ${message}`)
         }
       })
